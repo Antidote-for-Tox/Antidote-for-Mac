1c8aca624da7491050edb3892542a739a84b6b9a
Dmytro Vorobiov <d@dvor.me>
2016-11-26 21:38:16 +0100

    Initial commit

---
+++ Antidote/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Antidote
+//
+//  Created by Dmytro Vorobiov on 26/11/16.
+//  Copyright © 2016 Dmytro Vorobiov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,8 @@
+# Antidote for Mac
+
+[Tox](https://tox.chat/) client for macOS.
+
+## License
+
+Antidote is available under GNU General Public License v3.0. See the [LICENSE](LICENSE) file for more info.
+
