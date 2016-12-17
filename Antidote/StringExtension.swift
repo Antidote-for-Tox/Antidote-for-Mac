f26680777a42a9e3c20601eca8e5607062d0967c
Luppov Artur <luppoffart@icloud.com>
2016-12-17 23:34:20 +0500

    Basic setup, hierarchy and shared components

---
+++ Antidote/StringExtension.swift
@@ -0,0 +1,16 @@
+//
+//  StringExtension.swift
+//  Antidote
+//
+//  Created by Yoba on 18/12/2016.
+//  Copyright © 2016 Dmytro Vorobiov. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    init(localized: String, _ arguments: CVarArg...) {
+        let format = NSLocalizedString(localized, tableName: nil, bundle: Bundle.main, value: "", comment: "")
+        self.init(format: format, arguments: arguments)
+    }
+}
